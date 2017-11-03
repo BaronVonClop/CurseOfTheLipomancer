b30703d787bd6bc653143306b1a62d620d0decd5
BaronVonClop <baronvonclop@gmail.com>
2017-11-02 20:24:14 -0400

    Added navigation and the beginnings of a system to store rooms.

--- js/curse.js
+++ js/curse.js
@@ -15,6 +15,31 @@ var stamina;
 var wisdom;
 var charisma;
 
+var locations = {
+    "Orgrimmar":
+        {
+        "ValleyOfHonor":
+            {
+                "south":"TheDrag",
+                "east":"WyvernsTail",
+                "title":"Valley Of Honor",
+                "desc":"It's really fucking dusty, thanks Orgrimmar"
+            },
+        "TheDrag":
+            {
+                "north":"ValleyOfHonor",
+                "title":"The Drag",
+                "desc":"IT FUCKIN DARK HOMIE"
+            },
+        "WyvernsTail":
+            {
+                "west":"ValleyOfHonor",
+                "title":"Wyvern's Tail",
+                "desc":"A shitty tavern."
+            }
+        }
+}
+
 function checkPointsRem(){
     strength = document.getElementById("strength").value;
     intellect = document.getElementById("intellect").value;
@@ -28,7 +53,6 @@ function checkPointsRem(){
 
     checkAbilities();
 }
-
 function checkAbilities(){
     document.getElementById("abmortalstrike").style.color = "red";
     document.getElementById("abcleave").style.color= "red";
@@ -40,47 +64,36 @@ function checkAbilities(){
         document.getElementById("abcleave").style.color= "green";
     }
 }
-
 function showMortalStrike(){
     document.getElementById("statdescbox").value = "Mortal Strike\n\nStrike your enemy with a heavy blow. Uses a two handed weapon.";
 }
-
 function showExpand(){
     document.getElementById("statdescbox").value = "Expand\n\nUsing magic, you have the ability to expand your armor and clothing to fit you no matter what your size is. You can ignore sizes when looting clothing, and will never outgrow what you are wearing, no matter how much weight is gained.";
 }
-
 function showSleightofHand(){
     document.getElementById("statdescbox").value = "Sleight of Hand\n\nYour hands are deft and easily able to grab small objects without anyone noticing. At certain times, you will be able to change or completely bypass events when you have this skill.";
 }
-
 function showConsume(){
     document.getElementById("statdescbox").value = "Consume\n\nYour ability to push aside stomach discomfort allows you to eat significantly more. This allows you to more easily bypass certain events, but is a double-edged sword; it also means you can be forced to eat more against your will, as well.";
 }
-
 function showStrength(){
     document.getElementById("statdescbox").value = "Strength\n\nStrength represents your physical ability with brute force. Almost all melee attacks use your Strength to determine how much damage is dealt.\n\nStrength also determines how much weight you can carry -- including your body weight. Higher strength allows you to gain more weight before feeling negative penalties, and allows you to remain mobile after being fattened extreme amounts.";
 }
-
 function showAgility(){
     document.getElementById("statdescbox").value = "Agility\n\nAgility represents your ability to move quickly and quietly, as well as deftness. Melee attacks with certain weapons use Agility instead of Strength, and while in combat, Agility determines your chance to dodge and enemy's attacks.\n\nAgility is heavily affected by weight; the fatter you are, the lower your agility. Agility also reduces the movement penalty when being too full.";
 }
-
 function showIntellect(){
     document.getElementById("statdescbox").value = "Intellect\n\nIntellect represents your book smarts. Magic attacks use your Intellect score to determine damage, and affects your maximum mana.\n\nIntellect is also used to identify magic items.";
 }
-
 function showWisdom(){
     document.getElementById("statdescbox").value = "Wisdom\n\nWisdom represents your street smarts. Healing and restorative magics use your Wisdom score to determine their effectiveness. In combat, Wisdom helps you to resist mind-altering effects, such as hunger spells.";
 }
-
 function showStamina(){
     document.getElementById("statdescbox").value = "Stamina\n\nStamina represents your endurance and ability to keep going. Your HP is based on stamina, as is your stomach capacity. The higher your stamina, the more you are able to consume in one sitting.";
 }
-
 function showCharisma(){
     document.getElementById("statdescbox").value = "Charisma\n\nCharisma represents your ability to talk and be social. Charisma affects your chances with conversation based abilities, used to end combat.\n\nCharisma also affects prices in shops; people naturally like you, and are willing to give lower prices.";
 }
-
 function ccdone(){
 
     pcname = document.getElementById("ccname").value;
@@ -113,8 +126,12 @@ function ccdone(){
     document.getElementById("charcreate").style.display = "none";
     begingame();
 }
-
 function begingame(){
     document.getElementById("statusname").innerHTML = pcname;
     document.getElementById("game").style.display="block";
+    loadRoom("Orgrimmar", "WyvernsTail");
+}
+function loadRoom(zone, room){
+    document.getElementById("deschead").innerHTML = locations[zone][room]["title"];
+    document.getElementById("descbody").innerHTML = locations[zone][room]["desc"];
 }
