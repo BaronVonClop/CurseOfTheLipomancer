b2a1fa317fab3316bc714bb026c7c2ad1d5610c5
Baron Von Clop <baronvonclop@gmail.com>
2017-11-15 13:12:12 -0500

    Condensing CC descriptions. Don't need a function for each one, that is silly.

--- js/curse.js
+++ js/curse.js
@@ -109,24 +109,42 @@ function showSleightofHand(){
 function showConsume(){
     document.getElementById("statdescbox").value = "Consume\n\nYour ability to push aside stomach discomfort allows you to eat significantly more. This allows you to more easily bypass certain events, but is a double-edged sword; it also means you can be forced to eat more against your will, as well.";
 }
-function showStrength(){
-    document.getElementById("statdescbox").value = "Strength\n\nStrength represents your physical ability with brute force. Almost all melee attacks use your Strength to determine how much damage is dealt.\n\nStrength also determines how much weight you can carry -- including your body weight. Higher strength allows you to gain more weight before feeling negative penalties, and allows you to remain mobile after being fattened extreme amounts.";
+
+function showCCDesc(id){
+
+    if (id == "ccstrength"){
+        document.getElementById("statdescbox").value = "Strength\n\nStrength represents your physical ability with" +
+            " brute force. Almost all melee attacks use your Strength to determine how much damage is dealt.\n\n" +
+            "Strength also determines how much weight you can carry -- including your body weight. Higher strength " +
+            "allows you to gain more weight before feeling negative penalties, and allows you to remain mobile after " +
+            "being fattened extreme amounts.";
+    } else if (id == "ccagility"){
+        document.getElementById("statdescbox").value = "Agility\n\nAgility represents your ability to move quickly" +
+            " and quietly, as well as deftness. Melee attacks with certain weapons use Agility instead of Strength," +
+            " and while in combat, Agility determines your chance to dodge and enemy's attacks.\n\nAgility is heavily" +
+            " affected by weight; the fatter you are, the lower your agility. Agility also reduces the movement" +
+            " penalty when being too full.";
+    } else if (id == "ccintellect"){
+        document.getElementById("statdescbox").value = "Intellect\n\nIntellect represents your book smarts. Magic" +
+            " attacks use your Intellect score to determine damage, and affects your maximum mana.\n\nIntellect is" +
+            " also used to identify magic items.";
+    } else if (id == "ccwisdom"){
+        document.getElementById("statdescbox").value = "Wisdom\n\nWisdom represents your street smarts. Healing and" +
+            " restorative magics use your Wisdom score to determine their effectiveness. In combat, Wisdom helps you" +
+            " to resist mind-altering effects, such as hunger spells.";
+    } else if (id == "ccstamina"){
+        document.getElementById("statdescbox").value = "Stamina\n\nStamina represents your endurance and ability to" +
+            " keep going. Your HP is based on stamina, as is your stomach capacity. The higher your stamina, the" +
+            " more you are able to consume in one sitting.";
+    } else if (id == "cccharisma"){
+        document.getElementById("statdescbox").value = "Charisma\n\nCharisma represents your ability to talk and be" +
+            " social. Charisma affects your chances with conversation based abilities, used to end combat.\n\n" +
+            "Charisma also affects prices in shops; people naturally like you, and are willing to give lower prices.";
+    } else {
+        document.getelementById("statdescbox").value = "???";
+    }
 }
-function showAgility(){
-    document.getElementById("statdescbox").value = "Agility\n\nAgility represents your ability to move quickly and quietly, as well as deftness. Melee attacks with certain weapons use Agility instead of Strength, and while in combat, Agility determines your chance to dodge and enemy's attacks.\n\nAgility is heavily affected by weight; the fatter you are, the lower your agility. Agility also reduces the movement penalty when being too full.";
-}
-function showIntellect(){
-    document.getElementById("statdescbox").value = "Intellect\n\nIntellect represents your book smarts. Magic attacks use your Intellect score to determine damage, and affects your maximum mana.\n\nIntellect is also used to identify magic items.";
-}
-function showWisdom(){
-    document.getElementById("statdescbox").value = "Wisdom\n\nWisdom represents your street smarts. Healing and restorative magics use your Wisdom score to determine their effectiveness. In combat, Wisdom helps you to resist mind-altering effects, such as hunger spells.";
-}
-function showStamina(){
-    document.getElementById("statdescbox").value = "Stamina\n\nStamina represents your endurance and ability to keep going. Your HP is based on stamina, as is your stomach capacity. The higher your stamina, the more you are able to consume in one sitting.";
-}
-function showCharisma(){
-    document.getElementById("statdescbox").value = "Charisma\n\nCharisma represents your ability to talk and be social. Charisma affects your chances with conversation based abilities, used to end combat.\n\nCharisma also affects prices in shops; people naturally like you, and are willing to give lower prices.";
-}
+
 function ccdone(){
 
     pcname = document.getElementById("ccname").value;
